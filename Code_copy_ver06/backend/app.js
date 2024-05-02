7aa02ecd237edd6251bd0651c8a62b9cd11637e3
thanhpham1017 <109201865+thanhpham1017@users.noreply.github.com>
2024-05-02 23:55:39 +0700

    Update app.js

--- Code_copy_ver06/backend/app.js
+++ Code_copy_ver06/backend/app.js
@@ -122,18 +122,18 @@ app.use('/auth', authRouter);
 
 // Deployment routes
 
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '/front-end/build')));
+// const __dirname1 = path.resolve();
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname1, '/front-end/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'front-end', 'build', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send("API is running success");
-    });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, 'front-end', 'build', 'index.html'));
+//     });
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send("API is running success");
+//     });
+// }
 
 // Deployment routes
 
